9877dc4bb8199fcb705389fb526a3e67da44b7b5
Hong Vu NGUYEN <vulocke@gmail.com>
2020-11-25 21:52:19 +0100

    CRUD

--- tests/test_views.py
+++ tests/test_views.py
@@ -10,5 +10,22 @@ class TestViews(TestCase):
     def test_products_json(self):
         response = self.client.get("/api/v1/products")
         products = response.json
-        self.assertIsInstance(products, dict)
+        self.assertIsInstance(products, list)
         self.assertGreater(len(products), 2) # 2 is not a mistake here.
+
+    def test_product_found(self):
+        response = self.client.get("/api/v1/products/1")
+        print(response.status_code)
+        product = response.json
+        self.assertIsInstance(product, dict)
+        self.assertEqual(product.get('id'), 1)
+
+    def test_product_not_found(self):
+        response = self.client.get("/api/v1/products/4")
+        print(response.status_code)
+        product = response.json
+        self.assertEqual(product, None)
+
+    def test_product_delete(self):
+        response = self.client.get("/api/v1/products/1")
+        print("code retour delete: " + str(response.status_code))
--- wsgi.py
+++ wsgi.py
@@ -1,18 +1,60 @@
 # wsgi.py
 # pylint: disable=missing-docstring
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, abort, request
+import itertools
+import json
+
 app = Flask(__name__)
+
+PRODUCTS = {
+	1: { 'id': 1, 'name': 'Skello' },
+	2: { 'id': 2, 'name': 'Socialive.tv' },
+	3: { 'id': 3, 'name': 'Product3' }
+}
 
 @app.route('/')
 def hello():
     return "Hello World!"
 
-@app.route('/api/v1/products')
-def get_products():
-	PRODUCTS = {
-		1: { 'id': 1, 'name': 'Skello' },
-		2: { 'id': 2, 'name': 'Socialive.tv' },
-		3: { 'id': 3, 'name': 'Product3' }
-	}
-	return jsonify(PRODUCTS)
+@app.route('/api/v1/products', methods=["GET"])
+def read_all_products():
+	return jsonify(list(PRODUCTS.values())), 200
+
+@app.route('/api/v1/products/<int:id>', methods=["GET"])
+def read_one_product(id):
+	product = PRODUCTS.get(id)
+	if product is None:
+		abort(404)
+	return jsonify(product), 200
+
+@app.route('/api/v1/products/<int:id>', methods=["DELETE"])
+def delete_one_product(id):
+	product = PRODUCTS.pop(id, None)
+	if product is None:
+		abort(404)
+	return jsonify(product),204
+
+@app.route('/api/v1/products', methods=["POST"])
+def create_one_product():
+	START_INDEX = len(PRODUCTS) + 1
+	IDENTIFIER_GENERATOR = itertools.count(START_INDEX)
+	id = next(IDENTIFIER_GENERATOR)
+
+	# request.get_data : get body json
+	# json.loads : convert bytes to json
+	body = json.loads(request.get_data())
+	product = {id : {'id' : id, 'name' : body.get('name')}}
+	PRODUCTS.update(product)
+	return jsonify(product), 201
+
+@app.route('/api/v1/products/<int:id>', methods=["PATCH"])
+def update_one_product(id):
+	# request.get_data : get body json
+	# json.loads : convert bytes to json
+	body = json.loads(request.get_data())
+	# name for update
+	name = body.get('name')
+	product = {id : {'id' : id, 'name' : name}}
+	PRODUCTS.update(product)
+	return jsonify(product), 204
